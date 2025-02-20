d6bcef456cdf04d176a2009fca4eb5d31e49ca9c
iasakura <5370686+iasakura@users.noreply.github.com>
2025-02-21 01:05:16 +0900

    .

--- LeanYjs.lean
+++ LeanYjs.lean
@@ -7,6 +7,9 @@ def ActorId := Nat
 instance : LT ActorId where
   lt x y := Nat.lt x y
 
+instance ActorIdOfNat n: OfNat ActorId n where
+  ofNat := n
+
 inductive YjsItem (A : Type): Type where
 | item: (origin: YjsItem A) -> (rightOrigin: YjsItem A) -> ActorId -> A -> YjsItem A
 | first: YjsItem A
@@ -28,12 +31,12 @@ inductive YjsLessThan1 (A : Type): YjsItem A -> YjsItem A -> Prop where
 
 /- transitive closure -/
 inductive YjsLessThanTr {a : Type}: YjsItem a -> YjsItem a -> Prop where
-| base: forall (item1 item2: YjsItem a), YjsLessThan1 a item1 item2 -> YjsLessThanTr item1 item
+| base: forall (item1 item2: YjsItem a), YjsLessThan1 a item1 item2 -> YjsLessThanTr item1 item2
 | trans: forall (item1 item2 item3: YjsItem a), YjsLessThanTr item1 item2 -> YjsLessThan1 a item2 item3 -> YjsLessThanTr item1 item3
 
 inductive YjsLessThan {a : Type}: YjsItem a -> YjsItem a -> Prop where
 | ltOrigin: forall item1 item2, YjsLessThanTr item1 item2 -> YjsLessThan item1 item2
-| ltTr: forall item1 item2 item3, YjsLessThanTr item1 item2 -> YjsLessThanTr item2 item3 -> YjsLessThan item1 item3
+| ltTr: forall item1 item2 item3, YjsLessThan item1 item2 -> YjsLessThan item2 item3 -> YjsLessThan item1 item3
 | lt1: forall (left1 left2 right1 right2: YjsItem a) (id1 id2: ActorId) (c1 c2: a),
     item1 = YjsItem.item left1 right1 id1 c1
     -> item2 = YjsItem.item left2 right2 id2 c2
@@ -62,15 +65,53 @@ inductive YjsLessThan {a : Type}: YjsItem a -> YjsItem a -> Prop where
     -> item2 = YjsItem.item left right id2 c2
     -> id2 < id1
     -> YjsLessThan item2 item1
-/- ????????????????????? -/
+  -- same origin, different right origin
 | ltSame3: forall (left right1 right2: YjsItem a) (id1 id2: ActorId) (c1 c2: a),
-    item1 = YjsItem.item left right id1 c1
-    -> item2 = YjsItem.item left right id2 c2
-    /- item2 < right1 -/
+    item1 = YjsItem.item left right1 id1 c1
+    -> item2 = YjsItem.item left right2 id2 c2
+    -> id1 < id2
+    /- left < item2 < right1 -/
     -> YjsLessThan item2 right1
     -> YjsLessThan item1 right2
 
     -> YjsLessThan item1 item2
 
+namespace ex1
+  open Nat
+  def item1 := YjsItem.item YjsItem.first YjsItem.last 1 1
+  def item2 := YjsItem.item YjsItem.first YjsItem.last 2 2
 
-#check YjsLessThan.lt1
+  example : YjsLessThan item1 item2 :=
+  by
+    apply @YjsLessThan.ltSame1 (item1 := item1) (item2 := item2) <;> try rfl
+    simp [ActorId]
+end ex1
+
+namespace ex2
+  def item0 := YjsItem.item YjsItem.first YjsItem.last 0 0
+  def item1 := YjsItem.item YjsItem.first YjsItem.last 1 1
+  def item2 := YjsItem.item YjsItem.first item0 2 2
+
+  example : YjsLessThan item2 item1 :=
+  by
+    apply YjsLessThan.ltTr (item2 := item0)
+    . apply YjsLessThan.ltOrigin
+      apply YjsLessThanTr.base
+      apply YjsLessThan1.ltRightOrigin
+    . apply YjsLessThan.ltSame1 <;> try rfl
+      unfold ActorId
+      decide
+end ex2
+  example : Not (YjsLessThan item1 item2) :=
+  by
+    intro h
+    cases h
+    . case ltOrigin t =>
+      cases t
+      .
+    case ltTr => done
+    case lt1 => done
+    case lt2 => done
+    case ltSame1 => done
+    case ltSame2 => done
+    case ltSame3 => done
