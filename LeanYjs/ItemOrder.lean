6e0fd5977b632456bf49b3152f6c3ce60fd5d143
Izumi Asakura <5370686+iasakura@users.noreply.github.com>
2025-06-17 01:16:15 +0900

    remove trans

--- LeanYjs/ItemOrder.lean
+++ LeanYjs/ItemOrder.lean
@@ -14,7 +14,8 @@ mutual
   inductive YjsLt {A : Type} (P : @ClosedPredicate A) : Nat -> YjsPtr A -> YjsPtr A -> Prop where
     | ltConflict h i1 i2 : ConflictLt P h i1 i2 -> YjsLt P (h + 1) i1 i2
     | ltOriginOrder i1 i2 : P.val i1 -> P.val i2 -> OriginLt _ i1 i2 -> YjsLt P 0 i1 i2
-    | ltTrans h1 h2 x (y : YjsItem A) z: YjsLt P h1 x y -> YjsLt P h2 y z -> YjsLt P (max h1 h2 + 1) x z
+    | ltOrigin h x o r id c : P.val (YjsItem.item o r id c) -> YjsLt P h x o -> YjsLt P (h + 1) x (YjsItem.item o r id c)
+    | ltRightOrigin h o r id c x : P.val (YjsItem.item o r id c) -> YjsLt P h r x -> YjsLt P (h + 1) (YjsItem.item o r id c) x
 
   inductive ConflictLt {A : Type} (P : @ClosedPredicate A) : Nat -> YjsPtr A -> YjsPtr A -> Prop where
     | ltOriginDiff h1 h2 h3 h4 l1 l2 r1 r2 id1 id2 c1 c2 :
@@ -44,24 +45,26 @@ lemma yjs_lt_p1_aux {A : Type} {P : @ClosedPredicate A} {h : Nat} : forall {x y 
         apply h ; assumption
       | ltOriginOrder x y _ =>
         assumption
-      | ltTrans h1 h2 x y z _ _ =>
-        have hlt_h : h1 < max h1 h2 + 1 := by
+      | ltOrigin h x o r id c hval hlt =>
+        have hlt_h : h < h + 1 := by
           omega
-        let ⟨ h, _ ⟩ := ih h1 hlt_h x y
-        tauto
+        let ⟨ ih, _ ⟩ := ih h hlt_h x o
+        apply ih; assumption
+      | ltRightOrigin h o r id c x hval hlt =>
+        assumption
     . intro hlt
       cases hlt with
       | ltOriginDiff h1 h2 h3 h4 l1 l2 r1 r2 id1 id2 c1 c2 hlt1 hlt2 hlt3 =>
         have hlt_h : h2 < max4 h1 h2 h3 h4 + 1 := by
           unfold max4
           omega
-        let ⟨ _, ih ⟩ := ih h2 hlt_h (YjsPtr.itemPtr (YjsItem.item l1 r1 id1 c1)) r2
-        tauto
+        let ⟨ ih, _ ⟩ := ih h2 hlt_h (YjsPtr.itemPtr (YjsItem.item l1 r1 id1 c1)) r2
+        apply ih hlt2
       | ltOriginSame h1 h2 l r1 r2 id1 id2 c1 c2 hlt1 hlt2 _ =>
         have hlt_h : h1 < max h1 h2 + 1 := by
           omega
-        let ⟨ _, ih ⟩ := ih h1 hlt_h (YjsPtr.itemPtr (YjsItem.item l r1 id1 c1)) r2
-        tauto
+        let ⟨ ih, _ ⟩ := ih h1 hlt_h (YjsPtr.itemPtr (YjsItem.item l r1 id1 c1)) r2
+        apply ih hlt1
 
 lemma yjs_lt_p1 {A : Type} {P : @ClosedPredicate A} {h : Nat} : forall {x y : YjsPtr A},
   YjsLt P h x y -> P.val x := by
