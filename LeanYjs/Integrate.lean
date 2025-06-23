19d27a80bc1b13f5176c0f0f61829a7a918fecc7
Izumi Asakura <5370686+iasakura@users.noreply.github.com>
2025-06-24 00:48:18 +0900

    add integrateProof.lean

--- LeanYjs/Integrate.lean
+++ LeanYjs/Integrate.lean
@@ -54,23 +54,3 @@ def integrate (newItem : YjsItem A) (arr : Array (YjsItem A)) : Except Integrate
       continue
 
   return (Array.insertIdxIfInBounds arr (Int.toNat destIdx) newItem)
-
-inductive ArrayPairwise {α : Type} (f : α -> α -> Prop) : Array α -> Prop where
-| empty : ArrayPairwise f #[] -- empty array is pairwise
-| push : forall (a : α) (arr : Array α),
-  ArrayPairwise f arr -> (forall b: α, b ∈ arr -> f b a)
-  -> ArrayPairwise f (Array.push arr a) -- if the tail is pairwise, then adding an element is pairwise
-
-theorem integrate_sound (A: Type) [BEq A] (newItem : YjsItem A) (arr newArr : Array (YjsItem A)) h1 :
-  ArrayPairwise (fun (x y : YjsItem A) => @YjsLt A h1 x y) arr
-  -> integrate newItem arr = Except.ok newArr
-  -> ∃ h2, ArrayPairwise (fun (x y : YjsItem A) => @YjsLt A h2 x y) newArr := by
-  sorry
-
-theorem integrate_commutative (A: Type) [BEq A] (a b : YjsItem A) (arr1 arr2 arr3 arr2' arr3' : Array (YjsItem A)) :
-  integrate a arr1 = Except.ok arr2
-  -> integrate b arr2 = Except.ok arr3
-  -> integrate b arr1 = Except.ok arr2'
-  -> integrate a arr2' = Except.ok arr3'
-  -> arr3 = arr3' := by
-  sorry
