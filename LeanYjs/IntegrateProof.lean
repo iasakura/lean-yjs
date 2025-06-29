0c67c3291e89311469cc90ddfb2bc1894351eb49
iasakura <5370686+iasakura@users.noreply.github.com>
2025-06-29 22:17:46 +0900

    same set lemma

--- LeanYjs/IntegrateProof.lean
+++ LeanYjs/IntegrateProof.lean
@@ -7,305 +7,10 @@ import LeanYjs.Totality
 import LeanYjs.Transitivity
 import LeanYjs.AntiSymmetry
 import LeanYjs.Integrate
+import LeanYjs.YjsArray
 
 variable {A : Type}
 variable [BEq A]
-
-def ArrSet (arr : Array (YjsItem A)) : YjsPtr A -> Prop :=
-  fun a => match a with
-  | YjsPtr.itemPtr item => item ∈ arr
-  | YjsPtr.first => True
-  | YjsPtr.last => True
-
-def ArrSetClosed (arr : Array (YjsItem A)) : Prop :=
-  IsClosedItemSet (ArrSet arr)
-
-omit [BEq A] in lemma arr_set_closed_push (arr : Array (YjsItem A)) (item : YjsItem A) :
-  ArrSetClosed arr ->
-  ArrSet arr item.origin ->
-  ArrSet arr item.rightOrigin ->
-  ArrSetClosed (Array.push arr item) := by
-  intros hclosed horigin hright
-  constructor <;> try simp [ArrSet, IsClosedItemSet]
-  . intros o r id c hor
-    cases o with
-    | itemPtr item' =>
-      simp
-      cases hor with
-      | inl hitem =>
-        left
-        apply hclosed.closedLeft at hitem
-        assumption
-      | inr heq =>
-        subst heq
-        left
-        assumption
-    | first =>
-      simp
-    | last =>
-      simp
-  . intros o r id c hor
-    cases r with
-    | itemPtr item' =>
-      simp
-      cases hor with
-      | inl hitem =>
-        left
-        apply hclosed.closedRight at hitem
-        assumption
-      | inr heq =>
-        subst heq
-        left
-        assumption
-    | first =>
-      simp
-    | last =>
-      simp
-
-omit [BEq A] lemma yjs_lt_mono (P Q : ItemSet A) (x y : YjsPtr A) :
-  IsClosedItemSet P ->
-  ItemSetInvariant P ->
-  (∀ a, P a -> Q a) ->
-  YjsLt P h x y ->
-  YjsLt Q h x y := by
-  intros hclosedP hinvP hsubset
-  intros hlt
-  revert hlt x y
-  apply Nat.strongRec' (p := fun h => ∀ x y, YjsLt P h x y → YjsLt Q h x y)
-  intros n ih x y hlt
-  cases hlt with
-  | ltOriginOrder _  _ _ _ hlt =>
-    apply YjsLt.ltOriginOrder
-    . apply hsubset ; assumption
-    . apply hsubset ; assumption
-    . assumption
-  | ltConflict _ _ _ hlt =>
-    cases hlt with
-    | ltOriginDiff =>
-      apply YjsLt.ltConflict
-      apply ConflictLt.ltOriginDiff
-      all_goals (apply ih; unfold max4; omega)
-      all_goals (assumption)
-    | ltOriginSame =>
-      apply YjsLt.ltConflict
-      apply ConflictLt.ltOriginSame <;> try assumption
-      all_goals (apply ih; omega)
-      all_goals (assumption)
-  | ltOrigin _ _ _ _ _ _ _ hlt =>
-    apply YjsLt.ltOrigin
-    . apply hsubset ; assumption
-    . cases hlt with
-      | leqSame =>
-        apply YjsLeq.leqSame
-        apply hsubset ; assumption
-      | leqLt =>
-        apply YjsLeq.leqLt
-        apply ih <;> try assumption
-        omega
-  | ltRightOrigin _ _ _ _ _ _ _ hlt =>
-    apply YjsLt.ltRightOrigin
-    . apply hsubset ; assumption
-    . cases hlt with
-      | leqSame =>
-        apply YjsLeq.leqSame
-        apply hsubset ; assumption
-      | leqLt =>
-        apply YjsLeq.leqLt
-        apply ih <;> try assumption
-        omega
-
-lemma yjs_lt'_mono :
-  ∀ (P Q : ItemSet A) (x y : YjsPtr A),
-  IsClosedItemSet P ->
-  ItemSetInvariant P ->
-  (∀ a, P a -> Q a) ->
-  YjsLt' P x y -> YjsLt' Q x y := by
-  intros P Q x y hclosedP hinvP hsubset hlt
-  obtain ⟨ _, hlt ⟩ := hlt
-  constructor
-  apply yjs_lt_mono P Q x y hclosedP hinvP hsubset hlt
-
-lemma yjs_leq'_mono :
-  ∀ (P Q : ItemSet A) (x y : YjsPtr A),
-  IsClosedItemSet P ->
-  ItemSetInvariant P ->
-  (∀ a, P a -> Q a) ->
-  YjsLeq' P x y -> YjsLeq' Q x y := by
-  intros P Q x y hclosedP hinvP hsubset hleq
-  obtain ⟨ _, hleq ⟩ := hleq
-  cases hleq with
-  | leqSame =>
-    exists 0
-    apply YjsLeq.leqSame
-    apply hsubset; assumption
-  | leqLt =>
-    constructor
-    apply YjsLeq.leqLt
-    apply yjs_lt_mono P Q x y hclosedP hinvP hsubset
-    assumption
-
-lemma push_subset {A} (arr : Array (YjsItem A)) (a : YjsItem A) :
-  ∀ x, ArrSet arr x -> ArrSet (Array.push arr a) x := by
-  intros x hin
-  unfold Array.push
-  unfold ArrSet at *
-  simp
-  cases x <;> (simp at *)
-  left; assumption
-
-lemma reachable_in {A} (arr : Array (YjsItem A)) (a : YjsPtr A) :
-  IsClosedItemSet (ArrSet arr) ->
-  ∀ x, OriginReachable a x -> ArrSet arr a -> ArrSet arr x := by
-  intros hclosed x hreach hin
-  induction hreach with
-  | reachable_head x a y step h ih =>
-    cases step with
-    | reachable =>
-      simp [ArrSet] at hin
-      apply hclosed.closedLeft at hin
-      apply ih
-      assumption
-    | reachable_right =>
-      simp [ArrSet] at hin
-      apply hclosed.closedRight at hin
-      apply ih
-      assumption
-  | reachable_single _ _ hstep =>
-    cases hstep with
-    | reachable =>
-      simp [ArrSet] at hin
-      apply hclosed.closedLeft at hin
-      assumption
-    | reachable_right =>
-      simp [ArrSet] at hin
-      apply hclosed.closedRight at hin
-      assumption
-
-lemma item_set_invariant_push (arr : Array (YjsItem A)) (item : YjsItem A) :
-  ItemSetInvariant (ArrSet arr) ->
-  ArrSetClosed arr ->
-  YjsLt' (ArrSet arr) item.origin item.rightOrigin ->
-  (∀ x, OriginReachable item x -> YjsLeq' (ArrSet arr) x item.origin ∨ YjsLeq' (ArrSet arr) item.rightOrigin x) ->
-  (∀ x : YjsItem A, ArrSet arr x -> x.id = item.id -> YjsLeq' (ArrSet arr) x item.origin ∨ YjsLeq' (ArrSet arr) item.rightOrigin x) ->
-  ItemSetInvariant (ArrSet (Array.push arr item)) := by
-  intros hinv hclosed horigin hreach hsameid
-  apply ItemSetInvariant.mk
-  . intros o r c id hitem
-    simp [ArrSet] at hitem
-    cases hitem with
-    | inl hin =>
-      apply hinv.origin_not_leq at hin
-      apply yjs_lt'_mono _ _ _ _  hclosed hinv _ hin
-      . intros a hlt
-        simp [ArrSet] at *
-        cases a <;> simp at *
-        left; assumption
-    | inr heq =>
-      subst heq
-      simp [YjsItem.origin, YjsItem.rightOrigin] at horigin
-      apply yjs_lt'_mono _ _ _ _  hclosed hinv _ horigin
-      . intros a hlt
-        simp [ArrSet] at *
-        cases a <;> simp at *
-        left; assumption
-  . intros o r c id x hin hreachable
-    simp [ArrSet] at hin
-    cases hin with
-    | inl hin =>
-      obtain hor := hinv.origin_nearest_reachable _ _ _ _ _ hin hreachable
-      cases hor with
-      | inl hor =>
-        left
-        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
-        intros
-        apply push_subset; assumption
-      | inr hor =>
-        right
-        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
-        intros
-        apply push_subset; assumption
-    | inr heq =>
-      subst heq
-      simp [YjsItem.origin, YjsItem.rightOrigin] at *
-      cases hreach _ hreachable with
-      | inl hor =>
-        left
-        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
-        intros
-        apply push_subset; assumption
-      | inr hor =>
-        right
-        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
-        intros
-        apply push_subset; assumption
-  . intros x y hinx hiny hineq hid
-    simp [ArrSet] at *
-    cases hinx with
-    | inl hinx =>
-      cases hiny with
-      | inl hiny =>
-        obtain hor := hinv.same_id_ordered _ _ hinx hiny hineq hid
-        cases hor with
-        | inl hleq =>
-          left
-          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-          apply push_subset
-        | inr hleq =>
-          right
-          cases hleq with
-          | inl hleq =>
-            left
-            apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-            apply push_subset
-          | inr hleq =>
-            right
-            cases hleq with
-            | inl hleq =>
-              left
-              apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-              apply push_subset
-            | inr hleq =>
-              right
-              apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-              apply push_subset
-      | inr hleq =>
-        subst hleq
-        cases hsameid _ hinx hid with
-        | inl hleq =>
-          left
-          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-          apply push_subset
-        | inr hleq =>
-          right
-          right
-          right
-          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-          apply push_subset
-    | inr hinx =>
-      subst hinx
-      cases hiny with
-      | inl hiny =>
-        cases hsameid _ hiny (by rw [hid]) with
-        | inl hleq =>
-          right
-          left
-          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-          apply push_subset
-        | inr hleq =>
-          right
-          right
-          left
-          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
-          apply push_subset
-      | inr heqy =>
-        subst heqy
-        cases hineq (refl _)
-
-inductive ArrayPairwise {α : Type} (f : α -> α -> Prop) : Array α -> Prop where
-| empty : ArrayPairwise f #[] -- empty array is pairwise
-| push : ∀ (a : α) (arr : Array α),
-  ArrayPairwise f arr -> (∀ b : α, b ∈ arr -> f b a)
-  -> ArrayPairwise f (Array.push arr a) -- if the tail is pairwise, then adding an element is pairwise
 
 lemma ok_bind {α β ε : Type} (x : α) (f : α -> Except β ε) :
   (do
@@ -373,9 +78,9 @@ def loop_invariant (P : ItemSet A) (arr : Array (YjsItem A)) (newItem : YjsItem 
   (isDone -> ∃ item : YjsItem A, arr[current]? = some item ∧ YjsLt' P item newItem)
 
 theorem integrate_sound (A: Type) [BEq A] (P : ItemSet A) (inv : ItemSetInvariant P) (newItem : YjsItem A) (arr newArr : Array (YjsItem A)) :
-  ArrayPairwise (fun (x y : YjsItem A) => YjsLt' P x y) arr
+  YjsArrInvariant arr.toList
   -> integrate newItem arr = Except.ok newArr
-  -> ArrayPairwise (fun (x y : YjsItem A) => YjsLt' P x y) newArr := by
+  -> YjsArrInvariant newArr.toList := by
   intros hsorted hintegrate
   unfold integrate at hintegrate
 
@@ -390,30 +95,31 @@ theorem integrate_sound (A: Type) [BEq A] (P : ItemSet A) (inv : ItemSetInvarian
   simp at hintegrate
 
   generalize hloop :
-    @forIn (Except IntegrateError) (List ℕ) ℕ instForInOfForIn' (MProd ℕ Bool) Except.instMonad
-    (List.range' (leftIdx.toNat + 1) (rightIdx.toNat - (leftIdx.toNat + 1)) 1) ⟨leftIdx.toNat + 1, false⟩ (fun i r => do
+    forIn (m := Except IntegrateError) (β := MProd _ _) (List.range' (leftIdx.toNat + 1) (rightIdx.toNat - (leftIdx.toNat + 1)) 1) ⟨leftIdx.toNat + 1, false⟩
+      (fun i r => do
       let other ← getExcept arr i
       if r.snd = false then do
           let oLeftIdx ← findIdx other.origin arr
           let oRightIdx ← findIdx other.rightOrigin arr
-          if oLeftIdx < max leftIdx 0 then pure (ForInStep.done ⟨i, r.snd⟩)
+          if oLeftIdx < leftIdx ∨ oLeftIdx < 0 then pure (ForInStep.done ⟨i, r.snd⟩)
             else
-              if oLeftIdx = max leftIdx 0 then
+              if oLeftIdx = leftIdx ⊔ 0 then
                 if other.id < newItem.id then pure (ForInStep.yield ⟨i, false⟩)
                 else
-                  if oRightIdx = max rightIdx 0 then pure (ForInStep.done ⟨i, r.snd⟩) else pure (ForInStep.yield ⟨i, true⟩)
+                  if oRightIdx = rightIdx ⊔ 0 then pure (ForInStep.done ⟨i, r.snd⟩)
+                  else pure (ForInStep.yield ⟨i, true⟩)
               else pure (ForInStep.yield ⟨i, r.snd⟩)
         else do
           let oLeftIdx ← findIdx other.origin arr
           let oRightIdx ← findIdx other.rightOrigin arr
-          if oLeftIdx < max leftIdx 0 then pure (ForInStep.done ⟨r.fst, r.snd⟩)
+          if oLeftIdx < leftIdx ∨ oLeftIdx < 0 then pure (ForInStep.done ⟨r.fst, r.snd⟩)
             else
-              if oLeftIdx = max leftIdx 0 then
+              if oLeftIdx = leftIdx ⊔ 0 then
                 if other.id < newItem.id then pure (ForInStep.yield ⟨r.fst, false⟩)
                 else
-                  if oRightIdx = max rightIdx 0 then pure (ForInStep.done ⟨r.fst, r.snd⟩)
+                  if oRightIdx = rightIdx ⊔ 0 then pure (ForInStep.done ⟨r.fst, r.snd⟩)
                   else pure (ForInStep.yield ⟨r.fst, true⟩)
-              else pure (ForInStep.yield (⟨r.fst, r.snd⟩ : MProd ℕ Bool))) = l at hintegrate
+              else pure (ForInStep.yield ⟨r.fst, r.snd⟩)) = l at hintegrate
 
   obtain ⟨ _ ⟩ | ⟨ res ⟩ := l; cases hintegrate
   apply for_in_list_loop_invariant (I := fun x state => loop_invariant P arr newItem rightIdx.toNat x state) at hloop
@@ -422,9 +128,11 @@ theorem integrate_sound (A: Type) [BEq A] (P : ItemSet A) (inv : ItemSetInvarian
   rw [<-hintegrate]
   -- Here, we prove that the array is still pairwise ordered after the integration.
   -- So, what we need is arr[res.first] < newItem < arr[res.first + 1] (and also, 0 <= res.first <= arr.size)
+  sorry
 
 theorem integrate_commutative (A: Type) [BEq A] (a b : YjsItem A) (arr1 arr2 arr3 arr2' arr3' : Array (YjsItem A)) :
-  integrate a arr1 = Except.ok arr2
+  YjsArrInvariant arr1.toList
+  -> integrate a arr1 = Except.ok arr2
   -> integrate b arr2 = Except.ok arr3
   -> integrate b arr1 = Except.ok arr2'
   -> integrate a arr2' = Except.ok arr3'
---
+++ LeanYjs/YjsArray.lean
@@ -0,0 +1,473 @@
+import LeanYjs.Item
+import LeanYjs.ItemSet
+import LeanYjs.ActorId
+import LeanYjs.ItemOrder
+import LeanYjs.ItemSetInvariant
+import LeanYjs.Totality
+import LeanYjs.Transitivity
+import LeanYjs.AntiSymmetry
+import LeanYjs.Integrate
+
+variable {A : Type}
+variable [BEq A]
+
+def ArrSet (arr : List (YjsItem A)) : YjsPtr A -> Prop :=
+  fun a => match a with
+  | YjsPtr.itemPtr item => item ∈ arr
+  | YjsPtr.first => True
+  | YjsPtr.last => True
+
+def ArrSetClosed (arr : List (YjsItem A)) : Prop :=
+  IsClosedItemSet (ArrSet arr)
+
+omit [BEq A] in lemma arr_set_closed_push (arr : List (YjsItem A)) (item : YjsItem A) :
+  ArrSetClosed arr ->
+  ArrSet arr item.origin ->
+  ArrSet arr item.rightOrigin ->
+  ArrSetClosed (item :: arr) := by
+  intros hclosed horigin hright
+  constructor <;> try simp [ArrSet, IsClosedItemSet]
+  . intros o r id c hor
+    cases o with
+    | itemPtr item' =>
+      simp
+      cases hor with
+      | inr hitem =>
+        right
+        apply hclosed.closedLeft at hitem
+        assumption
+      | inl heq =>
+        subst heq
+        right
+        assumption
+    | first =>
+      simp
+    | last =>
+      simp
+  . intros o r id c hor
+    cases r with
+    | itemPtr item' =>
+      simp
+      cases hor with
+      | inr hitem =>
+        right
+        apply hclosed.closedRight at hitem
+        assumption
+      | inl heq =>
+        subst heq
+        right
+        assumption
+    | first =>
+      simp
+    | last =>
+      simp
+
+omit [BEq A] lemma yjs_lt_mono (P Q : ItemSet A) (x y : YjsPtr A) :
+  IsClosedItemSet P ->
+  ItemSetInvariant P ->
+  (∀ a, P a -> Q a) ->
+  YjsLt P h x y ->
+  YjsLt Q h x y := by
+  intros hclosedP hinvP hsubset
+  intros hlt
+  revert hlt x y
+  apply Nat.strongRec' (p := fun h => ∀ x y, YjsLt P h x y → YjsLt Q h x y)
+  intros n ih x y hlt
+  cases hlt with
+  | ltOriginOrder _  _ _ _ hlt =>
+    apply YjsLt.ltOriginOrder
+    . apply hsubset ; assumption
+    . apply hsubset ; assumption
+    . assumption
+  | ltConflict _ _ _ hlt =>
+    cases hlt with
+    | ltOriginDiff =>
+      apply YjsLt.ltConflict
+      apply ConflictLt.ltOriginDiff
+      all_goals (apply ih; unfold max4; omega)
+      all_goals (assumption)
+    | ltOriginSame =>
+      apply YjsLt.ltConflict
+      apply ConflictLt.ltOriginSame <;> try assumption
+      all_goals (apply ih; omega)
+      all_goals (assumption)
+  | ltOrigin _ _ _ _ _ _ _ hlt =>
+    apply YjsLt.ltOrigin
+    . apply hsubset ; assumption
+    . cases hlt with
+      | leqSame =>
+        apply YjsLeq.leqSame
+        apply hsubset ; assumption
+      | leqLt =>
+        apply YjsLeq.leqLt
+        apply ih <;> try assumption
+        omega
+  | ltRightOrigin _ _ _ _ _ _ _ hlt =>
+    apply YjsLt.ltRightOrigin
+    . apply hsubset ; assumption
+    . cases hlt with
+      | leqSame =>
+        apply YjsLeq.leqSame
+        apply hsubset ; assumption
+      | leqLt =>
+        apply YjsLeq.leqLt
+        apply ih <;> try assumption
+        omega
+
+lemma yjs_lt'_mono :
+  ∀ (P Q : ItemSet A) (x y : YjsPtr A),
+  IsClosedItemSet P ->
+  ItemSetInvariant P ->
+  (∀ a, P a -> Q a) ->
+  YjsLt' P x y -> YjsLt' Q x y := by
+  intros P Q x y hclosedP hinvP hsubset hlt
+  obtain ⟨ _, hlt ⟩ := hlt
+  constructor
+  apply yjs_lt_mono P Q x y hclosedP hinvP hsubset hlt
+
+lemma yjs_leq'_mono :
+  ∀ (P Q : ItemSet A) (x y : YjsPtr A),
+  IsClosedItemSet P ->
+  ItemSetInvariant P ->
+  (∀ a, P a -> Q a) ->
+  YjsLeq' P x y -> YjsLeq' Q x y := by
+  intros P Q x y hclosedP hinvP hsubset hleq
+  obtain ⟨ _, hleq ⟩ := hleq
+  cases hleq with
+  | leqSame =>
+    exists 0
+    apply YjsLeq.leqSame
+    apply hsubset; assumption
+  | leqLt =>
+    constructor
+    apply YjsLeq.leqLt
+    apply yjs_lt_mono P Q x y hclosedP hinvP hsubset
+    assumption
+
+lemma push_subset {A} (arr : List (YjsItem A)) (a : YjsItem A) :
+  ∀ x, ArrSet arr x -> ArrSet (a :: arr) x := by
+  intros x hin
+  unfold ArrSet at *
+  simp
+  cases x <;> (simp at *)
+  right; assumption
+
+lemma reachable_in {A} (arr : List (YjsItem A)) (a : YjsPtr A) :
+  IsClosedItemSet (ArrSet arr) ->
+  ∀ x, OriginReachable a x -> ArrSet arr a -> ArrSet arr x := by
+  intros hclosed x hreach hin
+  induction hreach with
+  | reachable_head x a y step h ih =>
+    cases step with
+    | reachable =>
+      simp [ArrSet] at hin
+      apply hclosed.closedLeft at hin
+      apply ih
+      assumption
+    | reachable_right =>
+      simp [ArrSet] at hin
+      apply hclosed.closedRight at hin
+      apply ih
+      assumption
+  | reachable_single _ _ hstep =>
+    cases hstep with
+    | reachable =>
+      simp [ArrSet] at hin
+      apply hclosed.closedLeft at hin
+      assumption
+    | reachable_right =>
+      simp [ArrSet] at hin
+      apply hclosed.closedRight at hin
+      assumption
+
+lemma item_set_invariant_push (arr : List (YjsItem A)) (item : YjsItem A) :
+  ItemSetInvariant (ArrSet arr) ->
+  ArrSetClosed arr ->
+  YjsLt' (ArrSet arr) item.origin item.rightOrigin ->
+  (∀ x, OriginReachable item x -> YjsLeq' (ArrSet arr) x item.origin ∨ YjsLeq' (ArrSet arr) item.rightOrigin x) ->
+  (∀ x : YjsItem A, ArrSet arr x -> x.id = item.id -> YjsLeq' (ArrSet arr) x item.origin ∨ YjsLeq' (ArrSet arr) item.rightOrigin x) ->
+  ItemSetInvariant (ArrSet (item :: arr)) := by
+  intros hinv hclosed horigin hreach hsameid
+  apply ItemSetInvariant.mk
+  . intros o r c id hitem
+    simp [ArrSet] at hitem
+    cases hitem with
+    | inr hin =>
+      apply hinv.origin_not_leq at hin
+      apply yjs_lt'_mono _ _ _ _  hclosed hinv _ hin
+      . intros a hlt
+        simp [ArrSet] at *
+        cases a <;> simp at *
+        right; assumption
+    | inl heq =>
+      subst heq
+      simp [YjsItem.origin, YjsItem.rightOrigin] at horigin
+      apply yjs_lt'_mono _ _ _ _  hclosed hinv _ horigin
+      . intros a hlt
+        simp [ArrSet] at *
+        cases a <;> simp at *
+        right; assumption
+  . intros o r c id x hin hreachable
+    simp [ArrSet] at hin
+    cases hin with
+    | inr hin =>
+      obtain hor := hinv.origin_nearest_reachable _ _ _ _ _ hin hreachable
+      cases hor with
+      | inl hor =>
+        left
+        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
+        intros
+        apply push_subset; assumption
+      | inr hor =>
+        right
+        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
+        intros
+        apply push_subset; assumption
+    | inl heq =>
+      subst heq
+      simp [YjsItem.origin, YjsItem.rightOrigin] at *
+      cases hreach _ hreachable with
+      | inl hor =>
+        left
+        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
+        intros
+        apply push_subset; assumption
+      | inr hor =>
+        right
+        apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hor
+        intros
+        apply push_subset; assumption
+  . intros x y hinx hiny hineq hid
+    simp [ArrSet] at *
+    cases hinx with
+    | inr hinx =>
+      cases hiny with
+      | inr hiny =>
+        obtain hor := hinv.same_id_ordered _ _ hinx hiny hineq hid
+        cases hor with
+        | inl hleq =>
+          left
+          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+          apply push_subset
+        | inr hleq =>
+          right
+          cases hleq with
+          | inl hleq =>
+            left
+            apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+            apply push_subset
+          | inr hleq =>
+            right
+            cases hleq with
+            | inl hleq =>
+              left
+              apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+              apply push_subset
+            | inr hleq =>
+              right
+              apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+              apply push_subset
+      | inl hleq =>
+        subst hleq
+        cases hsameid _ hinx hid with
+        | inl hleq =>
+          left
+          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+          apply push_subset
+        | inr hleq =>
+          right
+          right
+          right
+          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+          apply push_subset
+    | inl hinx =>
+      subst hinx
+      cases hiny with
+      | inr hiny =>
+        cases hsameid _ hiny (by rw [hid]) with
+        | inl hleq =>
+          right
+          left
+          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+          apply push_subset
+        | inr hleq =>
+          right
+          right
+          left
+          apply yjs_leq'_mono _ _ _ _ hclosed hinv _ hleq
+          apply push_subset
+      | inl heqy =>
+        subst heqy
+        cases hineq (refl _)
+
+structure YjsArrInvariant (arr : List (YjsItem A)) : Prop where
+  closed : IsClosedItemSet (ArrSet arr)
+  item_set_inv : ItemSetInvariant (ArrSet arr)
+  sorted : List.Pairwise (fun (x y : YjsItem A) => YjsLt' (ArrSet arr) x y) arr
+  unique : List.Pairwise (fun x y => x ≠ y) arr
+
+lemma same_yjs_set_unique_aux (xs_all ys_all xs ys : List (YjsItem A)) :
+  YjsArrInvariant xs_all ->
+  YjsArrInvariant ys_all ->
+  (∀ a, ArrSet xs_all a ↔ ArrSet ys_all a) ->
+  (∃ t, t ++ xs = xs_all) ->
+  (∃ t, t ++ ys = ys_all) ->
+  (∀ x, ArrSet xs x ↔ ArrSet ys x) ->
+  xs = ys := by
+  intros hinv1 hinv2 hseteq_all hsubset1 hsubset2 hseteq
+  obtain ⟨ hclosed1, hinv1, hsort1, huniq1 ⟩ := hinv1
+  obtain ⟨ hclosed2, hinv2, hsort2, huniq2 ⟩ := hinv2
+
+  revert ys
+
+  induction xs with
+  | nil =>
+    intros ys hsubset2 hseteq
+    cases ys with
+    | nil => eq_refl
+    | cons y ys =>
+      obtain hitem := hseteq y
+      simp [ArrSet] at hitem
+  | cons x xs ih =>
+    intros ys hsubset2 hseteq
+    cases ys with
+    | nil =>
+      obtain hitem := hseteq x
+      simp [ArrSet] at hitem
+    | cons y ys =>
+      have hmin1 : ∀ a ∈ xs, YjsLt' (ArrSet xs_all) x a := by
+        obtain ⟨ t, heq ⟩ := hsubset1
+        subst heq
+        rw [List.pairwise_append] at hsort1
+        obtain ⟨ _, hsort1, _ ⟩ := hsort1
+        simp at hsort1
+        obtain ⟨ hsort1, _ ⟩ := hsort1
+        assumption
+      have hmin2 : ∀ a ∈ ys, YjsLt' (ArrSet ys_all) y a := by
+        obtain ⟨ t, heq ⟩ := hsubset2
+        subst heq
+        rw [List.pairwise_append] at hsort2
+        obtain ⟨ _, hsort2, _ ⟩ := hsort2
+        simp at hsort2
+        obtain ⟨ hsort2, _ ⟩ := hsort2
+        assumption
+
+      have heq : x = y := by
+        have hlt1 : x = y ∨ YjsLt' (ArrSet ys_all) y x := by
+          cases yjsItem_decidable_eq A x y with
+          | inl heq =>
+            left; assumption
+          | inr hneq =>
+            obtain ⟨ hinx, _ ⟩ := hseteq x
+            have hinx : x = y ∨ x ∈ ys:= by
+              simp [ArrSet] at hinx
+              assumption
+            cases hinx with
+            | inl heq =>
+              left; assumption
+            | inr hinx =>
+              apply hmin2 at hinx
+              obtain ⟨ _, hinx ⟩ := hinx
+              right
+              constructor
+              assumption
+
+        have hlt2 : x = y ∨ YjsLt' (ArrSet xs_all) x y := by
+          cases yjsItem_decidable_eq A x y with
+          | inl heq =>
+            subst heq; left; eq_refl
+          | inr hneq =>
+            obtain ⟨ _, hinx ⟩ := hseteq y
+            have hiny : y = x ∨ y ∈ xs:= by
+              simp [ArrSet] at hinx
+              assumption
+            cases hiny with
+            | inl heq =>
+              subst heq; left; eq_refl
+            | inr hiny =>
+              apply hmin1 at hiny
+              obtain ⟨ _, hiny ⟩ := hiny
+              right
+              constructor
+              assumption
+
+        cases hlt1 with
+        | inl heq =>
+          assumption
+        | inr hlt1 =>
+          cases hlt2 with
+          | inl heq =>
+            assumption
+          | inr hlt2 =>
+            have hlt3 : YjsLt' (ArrSet ys_all) x y := by
+              apply yjs_lt'_mono (ArrSet xs_all) _ _ _ hclosed1 hinv1 <;> try assumption
+              intros a hlt
+              rw [<-hseteq_all a]; assumption
+
+            cases yjs_lt_anti_symm hclosed2 hinv2 x y hlt3 hlt1
+
+      subst heq
+      congr
+
+      apply ih
+      . obtain ⟨ t, heq ⟩ := hsubset1
+        subst heq
+        exists (t ++ [x])
+        simp
+      . obtain ⟨ t, heq ⟩ := hsubset2
+        subst heq
+        exists (t ++ [x])
+        simp
+      . intros a
+        simp [ArrSet] at *
+        cases a with
+        | first =>
+          simp
+        | last =>
+          simp
+        | itemPtr a =>
+          specialize hseteq a
+          simp at *
+          obtain ⟨ t, heq ⟩ := hsubset1
+          subst heq
+          obtain ⟨ t', heq ⟩ := hsubset2
+          subst heq
+          rw [List.pairwise_append] at huniq1
+          rw [List.pairwise_append] at huniq2
+          simp at *
+          obtain ⟨ _, ⟨ huniq1, _ ⟩, _ ⟩ := huniq1
+          obtain ⟨ _, ⟨ huniq2, _ ⟩ , _ ⟩ := huniq2
+          constructor
+          . intros hin
+            obtain hneq := huniq1 _ hin
+            have h : a = x ∨ a ∈ ys := by
+              rw [<-hseteq]
+              right; assumption
+            cases h with
+            | inl heq =>
+              subst heq
+              cases hneq (refl _)
+            | inr hin =>
+              assumption
+          . intros hin
+            obtain hneq := huniq2 _ hin
+            have h : a = x ∨ a ∈ xs := by
+              rw [hseteq]
+              right; assumption
+            cases h with
+            | inl heq =>
+              subst heq
+              cases hneq (refl _)
+            | inr hin =>
+              assumption
+
+lemma same_yjs_set_unique (xs ys : List (YjsItem A)) :
+  YjsArrInvariant xs ->
+  YjsArrInvariant ys ->
+  (∀ a, ArrSet xs a ↔ ArrSet ys a) ->
+  xs = ys := by
+  intros hinv1 hinv2 hseteq
+  apply same_yjs_set_unique_aux xs ys xs ys hinv1 hinv2 hseteq
+  . exists []
+  . exists []
+  . apply hseteq
