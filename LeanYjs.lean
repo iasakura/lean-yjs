65d4406fbe441379f5e4f1e3e530ca2233e59f38
iasakura <5370686+iasakura@users.noreply.github.com>
2025-08-09 00:38:35 +0900

    fix LeanYjs.lean

--- LeanYjs.lean
+++ LeanYjs.lean
@@ -1,2 +1,14 @@
 -- This module serves as the root of the `LeanYjs` library.
 -- Import modules here that should be built as part of the library.
+import LeanYjs.ListExtra
+import LeanYjs.Item
+import LeanYjs.ItemSet
+import LeanYjs.ActorId
+import LeanYjs.ItemOrder
+import LeanYjs.ItemSetInvariant
+import LeanYjs.Totality
+import LeanYjs.Transitivity
+import LeanYjs.AntiSymmetry
+import LeanYjs.Integrate
+import LeanYjs.YjsArray
+import LeanYjs.IntegrateProof
