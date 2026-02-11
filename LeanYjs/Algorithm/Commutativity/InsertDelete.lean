6f3be216eb7416ab609ede48541ea98b5db4f81a
iasakura <5370686+iasakura@users.noreply.github.com>
2026-02-11 22:51:46 +0900

    insert/delete

--- LeanYjs/Algorithm/Commutativity/InsertDelete.lean
+++ LeanYjs/Algorithm/Commutativity/InsertDelete.lean
@@ -5,4 +5,5 @@ theorem integrateSafe_deleteById_commutative [DecidableEq A] {newItem : Integrat
   (do
     let newArr <- arr.insert newItem
     Except.ok (deleteById newArr id)) = ((deleteById arr id).insert newItem) := by
-  sorry
+  simp [YjsState.insert, deleteById]
+  rfl
